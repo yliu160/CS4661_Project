7e055c2fcbda887bab1a1183ccd88990226bace4
steventhestudent <sgonza272@calstatela.edu>
2024-12-07 15:55:13 -0800

    use X_train_balanced. 'Normalize/Scale data if needed' was using/setting X_train

--- DevelopedMethods/imports.py
+++ DevelopedMethods/imports.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import numpy as np
+
+# sklearn classifier's
 from sklearn.neighbors import KNeighborsClassifier # knn algo
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split # data splitter helper
-from sklearn.preprocessing import StandardScaler # normalize features
 from sklearn import linear_model # LogisticRegression LinearRegression
-from sklearn import metrics # accuracy score
-from sklearn.pipeline import make_pipeline
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+# sklearn misc.
+from sklearn.preprocessing import StandardScaler, LabelEncoder        # StandardScaler = normalize features             ——LabelEncoder =  Label Encoding (convert value into unique int) - good for ft. w/ many unique values (to not create many dummy columns)
+from sklearn import metrics                                           # accuracy score
+from sklearn.model_selection import cross_val_score, train_test_split # cross_val_score = average(repeatedly split dataset into training/testing, .fit(), accuracy_score(.predict())         ——train_test_split example:   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=x)
+from sklearn.pipeline import make_pipeline                            #
+
+# plotting
+import matplotlib.pyplot as plt                                       # matplotlib. (graphs/plots)
+import seaborn as sns                                                 # matplotlib alternative
 
 # defaults
 pd.set_option('display.max_columns', None) # don't limit # columns shown
